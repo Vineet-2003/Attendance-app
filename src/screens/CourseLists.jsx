9ecda84786ce27f23c8cb381a1694c675af0f972
Vineet-2003 <107121127@nitt.edu>
2024-07-02 03:37:17 +0530

    update courseList screen

--- src/screens/CourseLists.jsx
+++ src/screens/CourseLists.jsx
@@ -3,20 +3,61 @@
 // eslint-disable-next-line prettier/prettier
 /* eslint-disable jsx-quotes */
 /* eslint-disable prettier/prettier */
-import {StyleSheet, Text, View, Button, ScrollView} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
 
-// import components
-import Buttons from '../components/Buttons';
+const CourseLists = ({navigation}) => {
+  const courses = [
+    "Power electronics",
+    "DC machines",
+    "Power System Analysis",
+    "power electronics",
+    "DC machines",
+    "power System Analysis",
+    "power electronics",
+    "DC machines",
+    "power System Analysis",
+  ];
 
-
-const CourseLists = ({ navigation }) => {
   return (
-    <ScrollView style={styles.container}>
-      <View className='my-6 mx-2'>
-        <Buttons />
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View className="my-6 mx-2 flex flex-col space-y-4">
+          {courses.map((courseName, index) => (
+            <Pressable
+              key={index++}
+              onPress={() =>
+                navigation.navigate("Courses", {
+                  courseId: 124,
+                })
+              }
+            >
+              <View
+                className="border-gray-600 flex-1 justify-center"
+                style={styles.ButtonContainer}
+              >
+                <Text className="text-xl px-4" style={styles.text}>
+                  {courseName}
+                </Text>
+              </View>
+            </Pressable>
+          ))}
+        </View>
+      </ScrollView>
+      <TouchableOpacity
+        style={styles.floatingButton}
+        onPress={() => navigation.navigate("AddCourses")}
+      >
+        <Text className="text-7xl text-gray-300">+</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -24,7 +65,34 @@ export default CourseLists;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#EEEEEE',
-
-  }
+    flex: 1,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  floatingButton: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: "#222831",
+    justifyContent: "center",
+    alignItems: "center",
+    bottom: 20,
+    right: 150,
+    elevation: 8, // This adds shadow for Android
+    shadowColor: "#000", // This adds shadow for iOS
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+  },
+  ButtonContainer: {
+    height: 60,
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    elevation: 4,
+  },
+  text: {
+    color: "#393E46",
+  },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /* eslint-disable quotes */
 /* eslint-disable prettier/prettier */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}", "./src/**/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
